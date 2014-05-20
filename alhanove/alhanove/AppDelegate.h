71e27366a2ae2f3bbcdd7f58d4803563c4bf9210
Ghassan ALMarei <ghass.64@live.com>
2014-05-20 14:24:01 +0400

    initial commit for al hanove project

---
+++ alhanove/alhanove/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  alhanove
+//
+//  Created by GALMarei on 5/20/14.
+//  Copyright (c) 2014 Danat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class UIStoryboard;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIStoryboard *storyboardVC;
+
+@end
