000e0322ecab5ed7e4cb8318cd580bf04d9ae5da
Ghassan ALMarei <ghass.64@live.com>
2014-05-21 15:36:13 +0400

    add screen of userData with all data

--- alhanove/alhanove/Form/Controller/UserDataViewController.h
+++ alhanove/alhanove/Form/Controller/UserDataViewController.h
@@ -9,18 +9,24 @@
 #import <UIKit/UIKit.h>
 #import "SBPickerSelector.h"
 
-@interface UserDataViewController : UIViewController<SBPickerSelectorDelegate,EnhancedKeyboardDelegate>
+@interface UserDataViewController : UIViewController<SBPickerSelectorDelegate,EnhancedKeyboardDelegate,UIImagePickerControllerDelegate,UIActionSheetDelegate>
+
+@property (strong, nonatomic) IBOutlet UILabel *pageTitle;
 
 @property (nonatomic, strong) IBOutlet UITextField* NameText;
 @property (nonatomic, strong) IBOutlet UITextField* AddressText;
 @property (nonatomic, strong) IBOutlet UITextField* EmailText;
 @property (nonatomic, strong) IBOutlet UITextField* MobileText;
+@property (strong, nonatomic) IBOutlet UILabel *dateLbl;
 @property (nonatomic, strong) IBOutlet UIButton* dateBtn;
 @property (nonatomic, strong) IBOutlet UIButton* passportAttachBtn;
 @property (nonatomic, strong) IBOutlet UIButton* personalImgAttachBtn;
 @property (nonatomic, strong) IBOutlet UIButton* payNowBtn;
 @property (nonatomic, strong) IBOutlet UIButton* payLaterBtn;
 @property (nonatomic, strong) IBOutlet UILabel* PriceLbl;
+
+@property (strong, nonatomic) NSString *tripCost;
+
 
 - (IBAction)backInvoked:(id)sender;
 - (IBAction)showDateInvoked:(id)sender;
--- alhanove/alhanove/Form/Controller/UserDataViewController.m
+++ alhanove/alhanove/Form/Controller/UserDataViewController.m
@@ -10,8 +10,14 @@
 
 @interface UserDataViewController ()
 {
+    BOOL imageForPassport;
+    
     EnhancedKeyboard *enhancedKeyboard;
+    SBPickerSelector *BookDatePicker;
 
+    NSDate* chosenDate;
+    UIImage* passportImage;
+    UIImage* personalImage;
 }
 @end
 
@@ -33,7 +39,11 @@
     enhancedKeyboard = [[EnhancedKeyboard alloc] init];
     enhancedKeyboard.delegate = self;
 
+    [self.pageTitle setFont:[UIFont mediumGeSSOfSize:20]];
     
+    //prepare the pickers
+    [self preparePickers];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -42,35 +52,65 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Methods
+-(void)preparePickers
+{
+    BookDatePicker = [SBPickerSelector picker];
+    BookDatePicker.tag = 0;
+    BookDatePicker.pickerType = SBPickerSelectorTypeDate;
+    BookDatePicker.delegate = self;
+    BookDatePicker.doneButtonTitle = @"تم";
+    BookDatePicker.cancelButtonTitle = @"إلغاء";
+    
+    
+}
+
 #pragma mark - IBAction
 - (IBAction)backInvoked:(id)sender
 {
-    
+    [self performSegueWithIdentifier:@"BackToHome" sender:self];
 }
 
 - (IBAction)showDateInvoked:(id)sender
 {
-    
+    [BookDatePicker showPickerOver:self];
 }
 
 - (IBAction)passportAttachInvoked:(id)sender
 {
+    imageForPassport = YES;
+    UIActionSheet *as = [[UIActionSheet alloc] initWithTitle:nil
+                                                    delegate:self
+                                           cancelButtonTitle:@"إلغاء"
+                                      destructiveButtonTitle:nil
+                                           otherButtonTitles:@"من الكاميرا", @"من مكتبة الصور", nil];
     
+    [as showInView:self.view];
 }
 
 - (IBAction)personalImgInvoked:(id)sender
 {
+    imageForPassport = NO;
+    UIActionSheet *as = [[UIActionSheet alloc] initWithTitle:nil
+                                                    delegate:self
+                                           cancelButtonTitle:@"إلغاء"
+                                      destructiveButtonTitle:nil
+                                           otherButtonTitles:@"من الكاميرا", @"من مكتبة الصور", nil];
     
+    [as showInView:self.view];
 }
 
 - (IBAction)payNowInvoked:(id)sender
 {
-    
+    //show the credit card dialog page
+    [self performSegueWithIdentifier:@"showPayNow" sender:self];
 }
 
 - (IBAction)payLaterInvoked:(id)sender
 {
-    
+    // show pay later page
+    [self performSegueWithIdentifier:@"showPayLater" sender:self];
+
 }
 
 
@@ -79,6 +119,10 @@
 {
     [textField resignFirstResponder];
     return YES;
+}
+
+- (void) textFieldDidBeginEditing:(UITextField *)textField {
+    
 }
 
 #pragma mark - KSEnhancedKeyboardDelegate Protocol
@@ -104,11 +148,108 @@
     
     
 }
-- (void) textFieldDidBeginEditing:(UITextField *)textField {
-    UIView *paddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
-    textField.rightView = paddingView;
-    textField.rightViewMode = UITextFieldViewModeAlways;
+
+#pragma mark - UIActionSheetDelegate Method
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+        NSString *buttonTitle = [actionSheet buttonTitleAtIndex:buttonIndex];
+        if ([@"من الكاميرا" isEqualToString:buttonTitle]) {
+            [self takePhotoWithCamera];
+        }
+        else if ([@"من مكتبة الصور" isEqualToString:buttonTitle]) {
+            [self selectPhotoFromLibrary];
+        }
 }
+
+
+#pragma mark - UIImagePickerController
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    
+    // Access the uncropped image from info dictionary
+    UIImage *image = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+    if (imageForPassport)
+        [self.passportAttachBtn setBackgroundImage:image forState:UIControlStateNormal];
+    else
+        [self.personalImgAttachBtn setBackgroundImage:image forState:UIControlStateNormal];
+
+
+    // Dismiss controller
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    // Resize image
+    UIGraphicsBeginImageContext(CGSizeMake(640, 960));
+    [image drawInRect: CGRectMake(0, 0, 640, 960)];
+    UIImage *smallImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    // Upload image
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.05f);
+    [self uploadImage:imageData forPassport:imageForPassport];
+}
+
+- (void)uploadImage:(NSData *)imageData forPassport:(BOOL)forPassport
+{
+    
+}
+
+-(void) takePhotoWithCamera {
+    if ([UIImagePickerController isSourceTypeAvailable:
+         UIImagePickerControllerSourceTypeCamera] == YES){
+        // Create image picker controller
+        UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+        
+        // Set source to the camera
+        imagePicker.sourceType =  UIImagePickerControllerSourceTypeCamera;
+        
+        // Delegate is self
+        imagePicker.delegate = self;
+        imagePicker.allowsEditing = YES;
+        
+        // Show image picker
+        [self presentViewController:imagePicker animated:YES completion:nil];
+    }
+}
+
+-(void) selectPhotoFromLibrary {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary])
+    {
+        UIImagePickerController *picker = [[UIImagePickerController alloc]init];
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        picker.allowsEditing = YES;
+        picker.delegate = self;
+        
+        [self presentViewController:picker animated:YES completion:nil];
+        
+    }
+}
+
+
+
+#pragma mark - UIPicker
+//if your piker is a traditional selection
+-(void) SBPickerSelector:(SBPickerSelector *)selector dateSelected:(NSDate *)date
+{
+    if (selector.tag == 0) {
+        chosenDate = date;
+        NSDateFormatter* df = [[NSDateFormatter alloc]init];
+        [df setDateFormat:@"yyyy-MM-dd"];
+        [self.dateBtn setTitle:[df stringFromDate:date] forState:UIControlStateNormal];
+
+    }
+}
+
+//when picker value is changing
+-(void) SBPickerSelector:(SBPickerSelector *)selector intermediatelySelectedValue:(id)value atIndex:(NSInteger)idx
+{
+    
+}
+
+//if the user cancel the picker
+-(void) SBPickerSelector:(SBPickerSelector *)selector cancelPicker:(BOOL)cancel
+{
+    
+}
+
 /*
 #pragma mark - Navigation
 
